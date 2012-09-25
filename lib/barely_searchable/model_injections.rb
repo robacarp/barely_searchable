e0d65879250b5402c09a2bbf6af39d7b40183fac
Robert L. Carpenter <robacarp@gmail.com>
2012-09-25 16:41:08 -0600

    General code updates:
    - add the ability to specify if/unless conditions on search parameters
    - abstract out some of the logic parts into their own methods.

--- lib/barely_searchable/model_injections.rb
+++ lib/barely_searchable/model_injections.rb
@@ -2,32 +2,55 @@ module BarelySearchable
   module ModelInjections
 
     module ClassMethods
-      def searches_on *fields
+      def searches_on *fields, conditions
+        if conditions.kind_of? Symbol
+          fields.push conditions
+          conditions = {}
+        end
+
+        conditions = {} if conditions.nil?
+
         if @searches.nil?
           init_searches_on
         end
 
-        @searches << fields
-        @searches.flatten!
+        fields.map! {|field| [field, conditions]}
+        @searches.push *fields
       end
 
       def init_searches_on
         @searches = []
       end
 
+      def searches
+        @searches
+      end
+
       def search query, limit = nil
         where = '0'
-        like = '%' + query.to_s.gsub(' ','%') + '%'
 
-        @searches.each do |field|
-          field = field.to_s
+        @searches.each do |search_field|
+          field = search_field[0].to_s
+          hash = search_field[1]
+
+          #check to make sure the field actually exists on the table
           unless self.columns_hash.include? field
             raise NameError, "#{field} is not a valid column on #{self}"
           end
 
-          if [:integer, :decimal, :float].include? self.columns_hash[ field.to_s ].type
-            where += ' or ' + self.arel_table[field].eq( query ).to_sql
+          #cast the query to the right data type for this column
+          col_query = cast_to_column(query, field)
+
+          #check for an if-condition
+          next if ! hash[:if].nil? && ! hash[:if].call(col_query)
+
+          #check for an unless condition
+          next if ! hash[:unless].nil? && hash[:unless].call(col_query)
+
+          if equality_columns.include? column_to_type(field)
+            where += ' or ' + self.arel_table[field].eq( col_query ).to_sql
           else
+            like = '%' + col_query.to_s.gsub(' ','%') + '%'
             where += ' or ' + self.arel_table[field].matches( like ).to_sql
           end
         end
@@ -39,6 +62,32 @@ module BarelySearchable
         end
 
       end
+
+      private
+
+      def cast_to_column data, column
+        case column_to_type(column)
+        when :decimal, :float
+          data.to_f
+        when :integer
+          data.to_i
+        when :string, :text, :binary, :date, :datetime, :time, :timestamp
+          data.to_s
+        when :boolean
+          !!data
+        else
+          data
+        end
+      end
+
+      def column_to_type column
+        self.columns_hash[ column ].type
+      end
+
+      def equality_columns
+        [:integer, :decimal, :float, :boolean]
+      end
+
     end
 
     def self.included to
